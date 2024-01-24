23a85b688fa0ff641a97f30cbcad1b4376b1b99e
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2024-01-24 14:58:31 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     
+    int arr2[2][2]={1,2,3,4};
+    for (int j=0;j<=2;j++){
+        for(int k=0;k<=2;k++){
+          cout<<arr2[j][k]<<endl;
+        }
+    }
+}
+
